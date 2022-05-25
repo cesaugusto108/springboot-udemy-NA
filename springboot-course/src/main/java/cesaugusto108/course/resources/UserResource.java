015938f3b03d418a5bb793a549715bf1ed1750c3
Cesar Augusto <ces.augusto108@gmail.com>
2022-05-24 22:18:21 -0300

    User entity and resource

---
+++ springboot-course/src/main/java/cesaugusto108/course/resources/UserResource.java
@@ -0,0 +1,19 @@
+package cesaugusto108.course.resources;
+
+import cesaugusto108.course.entities.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+    @GetMapping
+    public ResponseEntity<User> findAll() {
+        User user = new User(1L, "Maria", "maria@email.com", "999999999", "12345");
+
+        return ResponseEntity.ok().body(user);
+    }
+}
