50c15d19d33cc02d6bd76a4c3a0a0210781d5dfe
Cesar Augusto <ces.augusto108@gmail.com>
2022-05-30 17:21:14 -0300

    Exception handling - update

--- springboot-course/src/main/java/cesaugusto108/course/services/UserService.java
+++ springboot-course/src/main/java/cesaugusto108/course/services/UserService.java
@@ -9,6 +9,7 @@ import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.List;
 import java.util.Optional;
 
@@ -51,8 +52,13 @@ public class UserService {
     }
 
     public User update(Long id, User user) {
-        User updatedUser = userRepository.getOne(id);
-        updateData(updatedUser, user);
+        User updatedUser = null;
+        try {
+            updatedUser = userRepository.getOne(id);
+            updateData(updatedUser, user);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException(id);
+        }
 
         return userRepository.save(updatedUser);
     }
